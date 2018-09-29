8bed42aaddab99b0a191fdd3e7d7dc372b23729e
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-09-29 21:59:43 +0700

    OutCreate : logic

--- src/pages/out-create/out-create.ts
+++ src/pages/out-create/out-create.ts
@@ -7,11 +7,28 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class OutCreatePage {
 
+  date:any;
+  value:any;
+  paket:any;
+  explanation:any;
+
+  submitted : boolean = false;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.date = new Date().toISOString();
+    this.date = this.date.substr(0, this.date.indexOf('T'));
+
+    this.paket="paket1"
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad OutCreatePage');
+  }
+
+  submit(){
+    this.submitted = true;
+    if(this.value)
+      this.navCtrl.pop();
   }
 
 }
